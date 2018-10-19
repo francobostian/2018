19446fff0c4c7735d2c56c9169792a71c8823207
franco bostiancic <francobostian@gmail.com>
2018-10-19 20:33:56 -0300

    entrega tp6, esperemo q ande

---
+++ tp6 bostiancic/src/objects.js
@@ -0,0 +1,9 @@
+function Person(id, nombre, edad, genero) {
+  this.id = id,
+    this.nombre = nombre,
+    this.edad = edad,
+    this.genero = genero
+}
+export {
+  Person
+} 
---
+++ tp6 bostiancic/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import PersonService from '@/services/personService'
+
+export default {
+  name: 'home'
+}
+</script>
